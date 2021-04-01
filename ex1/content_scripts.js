ab5ac812834f1f903947c9e938eb38a505a609ee
kajikentaro <k.kentarou0412@gmail.com>
2021-04-01 23:34:04 +0900

    practice update

--- ex1/content_scripts.js
+++ ex1/content_scripts.js
@@ -1,2 +1,9 @@
-$('body').prepend('<div class="txt">Hello World!</div>')
-console.log('拡張機能のデバック！')
+var new_element = document.createElement('p');//新しいp要素を作成
+new_element.innerHTML = 'Hello World!'//そこにHello World!っていうテキストを設定
+
+var container = document.getElementById('container');//containerというIDを持つ要素を検索
+var header = document.getElementById('header');//headerというIDを持つ要素を検索
+
+container.insertBefore(new_element, header);//containerの中で、headerの前にnew_elementを追加する。
+
+console.log('F12キーをおした先で見れるコメント')//コメントを出力(printfみたいな感じ)
